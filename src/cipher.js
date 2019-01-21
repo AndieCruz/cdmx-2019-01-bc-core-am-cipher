70f09ad52c8d8667a3f7e0d1ae824012d1c82de4
AndieCruz <anndiie06@gmail.com>
2019-01-21 16:19:40 -0600

    Implemente el Hacker edition y pasan los test

--- src/cipher.js
+++ src/cipher.js
@@ -6,7 +6,7 @@ window.cipher = {
     //capturar el offset
     let espacios = parseInt(offset);
     //cambiar mayusculas
-    let mayus= mensaje.toUpperCase();
+    let mayus= mensaje;
     //el lugar del mensaje descifrado
     let final="";
     //iterar la palabra
@@ -15,12 +15,25 @@ window.cipher = {
       let letra= mayus.charCodeAt(i);
       //identificar posicion de caracteres
       //encontrar el valor en el codigo ascii
-      //poner la formula
-      let formula = (letra- 65+espacios)%26+65;
-      //obtener letra del descifrado
-      let resultado = String.fromCharCode(formula);
-      //guardarlo en la variable final
-      final +=resultado;
+      //que salte los caracteres especiales y numeros
+      if(letra >=65 && letra <=90 ){
+        //poner la formula para mayusculas
+        let formula = (letra - 65+espacios)%26+65;
+        //obtener letra del descifrado
+        let resultado = String.fromCharCode(formula);
+        //que se sume la oración
+        final +=resultado;
+      } else if(letra >=97 && letra<=122){
+        //poner la formula para mayusculas
+        let formula = (letra - 97+espacios)%26+97;
+        //obtener letra del descifrado
+        let resultado = String.fromCharCode(formula);
+        //que se sume la oración
+        final +=resultado;
+      }else{
+        let resultado = String.fromCharCode(letra);
+        final +=resultado;
+      }
     }
   return  final;
 },
@@ -31,26 +44,33 @@ window.cipher = {
     //capturar el offset
     let espacios = offset;
     //cambiar mayusculas
-    let mayus= mensaje.toUpperCase();
+    let mayus= mensaje;
     //el lugar del mensaje descifrado
     let final="";
     //iterar la palabra
     for(let i=0; i<mayus.length; i++){
       //que agarre esa letra
-      let letra= mayus.charAt(i);
-      //identificar posicion de caracteres
-      //encontrar el valor en el codigo ascii
-      let valor=letra.charCodeAt(0);
-      //poner la formula
-      let formula = (valor+65-espacios)%26+65;
+    let letra= mayus.charCodeAt(i);
+
+    if(letra >=65 && letra <=90 ){
+      //poner la formula para mayusculas
+      let formula = (letra + 65-espacios)%26+65;
       //obtener letra del descifrado
       let resultado = String.fromCharCode(formula);
-      //guardarlo en la variable final
-      final = final+resultado;
+      //que se sume la oración
+      final +=resultado;
+    } else if(letra >=97 && letra<=122){
+      //poner la formula para mayusculas
+      let formula = (letra - 122 - espacios)%26+122;
+      //obtener letra del descifrado
+      let resultado = String.fromCharCode(formula);
+      //que se sume la oración
+      final +=resultado;
+    }else{
+      let resultado = String.fromCharCode(letra);
+      final +=resultado;
     }
-  return  final;
-  //insert.innerHTML = msj;
+  }
+return  final;
 }
 };
-  //insert.innerHTML = msj;
-  //console.log(insert);
